5b5f585493e80be72c0b7457c3aa34af7ada6938
saadkarim402 <saadkarim782@gmail.com>
2023-07-22 07:32:21 -0700

    c1

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import Login from "../features/auth/components/Login";
+function LoginPage() {
+    return ( <div>
+
+        <Login></Login>
+        <h1>admin</h1>
+        <h3>email:saadkarim782@gmail.com</h3>
+        <h3>password:Saad1234karim</h3>
+    </div>  );
+}
+
+export default LoginPage;
